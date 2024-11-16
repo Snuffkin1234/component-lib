cf570bb429c1cd8a8aaafe29638b426d21f3cc96
snuffkin1234 <snuffkin1234@gmail.com>
2024-11-15 16:14:01 -0800

    initial push

---
+++ src/lib/YouTube.svelte
@@ -0,0 +1,12 @@
+<!-- Based on a code template at https://blog.robino.dev/posts/svelte-component-library by Ross Robino -->
+<script lang="ts">
+	let uid: string ="gouiY85kD2o";
+    let title: string="Renegade - Kevin Olusola";
+</script>
+<iframe
+	src="https://www.youtube.com/embed/{uid}"
+	{title}
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+	allowfullscreen
+></iframe>
